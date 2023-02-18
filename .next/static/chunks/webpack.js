c1f3c25462c3aa9031dd85b90b44c354659c234c
ditarahma08 <ditarahma08@gmail.com>
2023-02-18 15:53:32 +0700

    add font family

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8ff63ab53b5c7aa7"; }
+/******/ 		__webpack_require__.h = function() { return "9a4d5da73ba17760"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
